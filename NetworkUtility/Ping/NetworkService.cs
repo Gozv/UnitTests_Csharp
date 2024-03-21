3105a26734cf3cef2b75076a4aeac5e87f83a7e5
Gonzalo <gonzalosantacruzariel@gmail.com>
2024-03-21 11:57:35 -0300

    Testing Objects

--- NetworkUtility/Ping/NetworkService.cs
+++ NetworkUtility/Ping/NetworkService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,5 +21,42 @@ namespace NetworkUtility.Ping
         {
             return a + b;
         }
+
+        public DateTime LastPingDate()
+        {
+            return DateTime.Now;
+        }
+
+        public PingOptions GetPingOptions()
+        {
+            return new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+        }
+
+        public IEnumerable<PingOptions> MostRecentPings()
+        {
+            IEnumerable<PingOptions> pingOptions = new[]
+            {
+                new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            },
+                new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            },
+                new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            },
+            };
+            return pingOptions;
+        } 
     }
 }
